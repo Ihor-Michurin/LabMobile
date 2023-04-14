c37edb26861fe59bc72bc97b50f377d08d06e23c
Ihor Michurin <imichurin@teamworkcommerce.com>
2023-04-14 17:58:03 +0300

    Add empty project

---
+++ LabMobile/LabMobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LabMobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ LabMobile/LabMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LabMobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
